5a3e956399356a57a8e09bd78a152d6395b6cbc4
DragutinMil <dragutinmiladinovic@yahoo.com>
2025-06-05 17:08:55 +0200

    change_state

--- apps/chessroulette-web/modules/Match/containers/MatchStateDialogContainer/useTokenCheck.ts
+++
@@ -1,4 +0,0 @@
-import { useToken} from '@app/hooks/useToken';
-
-// TODO: This will be able to get the theme from the user settings or local storage in the future
-export const useTokenCheck = () => useToken();
--- apps/chessroulette-web/modules/Room/activities/Match/movex/reducer.ts
+++ apps/chessroulette-web/modules/Room/activities/Match/movex/reducer.ts
@@ -2,13 +2,33 @@ import { MovexReducer } from 'movex-core-util';
 import { ActivityActions, type ActivityState } from '../../movex';
 import { initialMatchActivityState } from './state';
 import * as MatchStore from '@app/modules/Match/movex';
-
+import counterReducer, {
+  CounterActions,
+} from '../counter';
 export const reducer: MovexReducer<ActivityState, ActivityActions> = (
   prev: ActivityState = initialMatchActivityState,
   action: ActivityActions
 ): ActivityState => {
-  
+  console.log('prev room ativityState', prev)
   if (prev.activityType === 'match') {
+    const isCounterAction =
+      action.type === 'increment' ||
+      action.type === 'decrement' ||
+      action.type === 'change' ||
+      action.type === 'incrementBy';
+      console.log('paprika')
+if (isCounterAction) {
+   console.log('paprika')
+      return {
+        ...prev,
+        increment: counterReducer(
+          { count: prev.increment },
+          action as CounterActions
+        ).count,
+      };
+}
+
+
     return {
       ...prev,
       activityState: MatchStore.reducer(
@@ -24,6 +44,7 @@ if (MatchStore.reducer.$transformState) {
   const matchStateTransformer = MatchStore.reducer.$transformState;
 
   reducer.$transformState = (state, masterContext): ActivityState => {
+    
     if (state.activityType === 'match') {
       return {
         ...state,
