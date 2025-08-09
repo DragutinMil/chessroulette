4654e982b729e567b336283e4f729723524844fa
DragutinMil <dragutinmiladinovic@yahoo.com>
2025-08-09 19:40:34 +0200

    ai puzzle redesign

--- apps/chessroulette-web/components/BoardEditor/BoardEditor.tsx
+++ apps/chessroulette-web/components/BoardEditor/BoardEditor.tsx
@@ -197,6 +197,7 @@ export const BoardEditor = ({
             <ChessboardContainer
               fen={fen}
               boardTheme={boardTheme}
+              onChangePuzzleAnimation={false}
               {...props}
               onMove={(m) => {
                 // Should this be part of the ChessFen or outside? The king not being removed
