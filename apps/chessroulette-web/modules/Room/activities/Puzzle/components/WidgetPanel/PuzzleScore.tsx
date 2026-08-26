52188b511b4cb2ccbec35038f2d3a447e1934882
DragutinMil <dragutinmiladinovic@yahoo.com>
2026-08-26 09:37:11 +0200

    shakinh fix

--- apps/chessroulette-web/modules/Room/activities/Puzzle/components/WidgetPanel/PuzzleScore.tsx
+++ apps/chessroulette-web/modules/Room/activities/Puzzle/components/WidgetPanel/PuzzleScore.tsx
@@ -299,12 +299,12 @@ const PuzzleScore = ({ chessAiMode, puzzle_rating, isTablet }: Props) => {
             <div className="relative">
               <span
                 key={value}
-                className={`${styles.flipInner} ${flipping ? styles.flip : ''}`}
+                // className={`${styles.flipInner} ${flipping ? styles.flip : ''}`}
               >
                 {value}
               </span>
 
-              {/* {showChange &&
+              {showChange &&
                 change !== 0 &&
                 (change > 0 ? (
                   <span className="absolute -bottom-1  ml-1 text-green-400 text-[18px] font-bold animate-fadeUp">
@@ -314,7 +314,7 @@ const PuzzleScore = ({ chessAiMode, puzzle_rating, isTablet }: Props) => {
                   <span className="absolute -top-4  ml-1 text-red-500 text-[18px] font-bold animate-fadeUp">
                     {change}
                   </span>
-                ))} */}
+                ))}
             </div>
             <span
               className={`flex items-center justify-center font-bold text-green-400 md:text-lg text-base  
